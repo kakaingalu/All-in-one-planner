2bd418b662f2ce5a0819100f844c35874c9cec0b
Joseph Kakai <josephngalu96gmail.com>
2022-12-19 11:01:22 +0300

    Added  filtodo list

---
+++ todo.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+class todo:
+    def__init__(self, root):
+        pass
+
+    def main():
+        root = tk():
+        ui = todo(root)
+        root.mainloop()
+
+    if __name__ == "__main__":
+        main()
